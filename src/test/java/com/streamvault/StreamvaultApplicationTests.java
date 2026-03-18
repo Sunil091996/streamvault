27535daed8b09de1499b583ba8259e0f07e6dec2
Sunil091996 <39492854+Sunil091996@users.noreply.github.com>
2026-03-18 20:05:33 +0530

    Created new spring boot project

---
+++ src/test/java/com/streamvault/StreamvaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streamvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamvaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
